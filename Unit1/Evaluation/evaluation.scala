1167761782b6c5ec96cacc6f77cd1df531f4d5f3
Ale2C <alejandro.castro17@tectijuana.edu.mx>
2021-04-14 18:20:55 -0700

    Add code of problem 2 - Scala file

--- Unit1/Evaluation/evaluation.scala
+++ Unit1/Evaluation/evaluation.scala
@@ -3,4 +3,7 @@ import org.apache.spark.sql.SparkSession
 
 val spark = SparkSession.builder().getOrCreate()
 
+// 2.- Load the Netflix Stock CSV file, make Spark infer the data types.
+
+val df = spark.read.option("header", "true").option("inferSchema","true")csv("Netflix_2011_2016.csv")
 
